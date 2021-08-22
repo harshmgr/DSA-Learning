f9197de77fb87717460d9e30c24f58bd464d262d
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2021-08-23 00:05:27 +0530

    Array Questions

--- BasicQuestion/BinarySearch.java
+++ BasicQuestion/BinarySearch.java
@@ -1,23 +1,32 @@
-/**
- * @(#)BinarySearch.java
- *
- *
- * @author 
- * @version 1.00 2021/8/22
- */
-
+import java.util.*;
 public class BinarySearch {
-        
-    /**
-     * Creates a new instance of <code>BinarySearch</code>.
-     */
-    public BinarySearch() {
-    }
-    
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+        	a[i]=sc.nextInt();
+        }
+        System.out.println("Enter element to search:");
+        int search=sc.nextInt();
+        int low=0;
+        int high=a.length-1;
+        while(low<=high){
+        	int mid=(low+high)/2;
+        	if(a[mid]>search){
+        		high=mid-1;
+        	}
+        	else if(a[mid]<search){
+        		low=mid+1;
+        		}
+        	else{
+        		System.out.println("Element found at: "+mid);
+        		return;
+        		}
+        }
+       System.out.println("Not found");
+        	
+        	
     }
 }
