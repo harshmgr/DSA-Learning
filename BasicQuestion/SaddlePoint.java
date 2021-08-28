4f6df1ef093b8048688fe7ea0b4152790298bc9c
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2021-08-29 00:16:18 +0530

    2D Array Problems

---
+++ BasicQuestion/SaddlePoint.java
@@ -0,0 +1,41 @@
+/**
+ * @(#)Saddle Point.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/8/28
+ */
+import java.util.*;
+public class SaddlePoint {
+	public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[][]=new int[n][n];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        
+        for(int i=0;i<arr.length;i++){
+            int svj=0;
+            for(int j=1;j<arr[0].length;j++){
+                if(arr[i][j]<arr[i][svj]){
+                    svj=j;
+                }
+            }
+            boolean flag=true;
+            for(int k=0;k<arr.length;k++){
+                if(arr[k][svj]>arr[i][svj]){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag==true){
+                System.out.println(arr[i][svj]);
+                return;
+            }
+        }
+        System.out.println("Invalid input");
+    }
+}
---
+++ BasicQuestion/Sorted2DArraySearch.java
@@ -0,0 +1,35 @@
+/**
+ * @(#)Sorted2DArraySearch.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/8/29
+ */
+import java.util.*;
+public class Sorted2DArraySearch {
+	 public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[][]=new int[n][n];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int x=sc.nextInt();
+        int i=0;
+        int j=arr.length-1;
+        while(i<arr.length && j>=0){
+            if(x==arr[i][j]){
+                System.out.println(i);
+                System.out.println(j);
+                return ;
+            }else if(x<arr[i][j]){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        System.out.println("Not Found");
+    }
+}
