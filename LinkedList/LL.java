c86be1e1b0bac033df5f63fe5ba6bb3586d30469
Harsh <kumarharsh8549@gmail.com>
2021-12-30 21:20:50 +0530

    LinkedList

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -41,7 +41,45 @@ public class LL {
 			}
 			System.out.println("Size is:"+size);
 		}
+		
+		void removefirst(){
+		head=head.next;
+		size--;
 	}
+	void getFirst(){
+		if(size==0){
+			System.out.println("Linked list Empty");
+		}else{
+			System.out.println("first data is: "+head.data);
+		}
+	}
+	void getLast(){
+			if(size==0){
+			System.out.println("Linked list Empty");
+		}else{
+			System.out.println("Last data is "+tail.data);
+		}
+	}
+	
+	int getAt(int index){
+		if(size==0){
+			System.out.println("Empty");
+			return -1;
+		}else if(size<index || index<0){
+			System.out.println("Not valid index number");
+			return -1;
+		}else{
+			Node temp=head;
+			for(int i=0;i<index;i++){
+				temp=temp.next;
+			}
+			return temp.data;
+		}
+	}
+}
+	
+	
+	
 
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
@@ -50,5 +88,10 @@ public class LL {
         ll.addLast(52);
         ll.addLast(12);
         ll.printList();
+        ll.removefirst();
+        ll.printList();
+        ll.getFirst();
+        ll.getLast();
+        System.out.println("Data at index: "+ll.getAt(5));
     }
 }
